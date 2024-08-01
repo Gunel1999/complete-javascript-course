218338a795190f0c66485d5e6a15deb2987ffb09
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-08-01 17:11:06 +0400

    Guess my number

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (arr) {
+  let text = '';
+  for (let i = 0; i < arr.length; i++) {
+    text += `... ${arr[i]}°C in ${i + 1} ${i + 1 === 1 ? 'day' : 'days'} `;
+    if (i === arr.length - 1) text += '...';
+  }
+  return text;
+};
+
+const testA = printForecast([17, 21, 23]);
+const testB = printForecast([12, 5, -5, 0, 4]);
+
+console.log(testA);
+console.log(testB);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// document.querySelector('.message').textContent = 'Correct number! 🎉';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 25;
+
+// document.querySelector('.guess').value = 1;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = +document.querySelector('.guess').value;
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number⛔';
+  }
+});
