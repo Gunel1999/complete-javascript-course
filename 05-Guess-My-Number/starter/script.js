e8355e808342db06accc46c75b84ca5e7f567911
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-08-02 17:40:27 +0400

    Guess my number finished

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,14 +1,66 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct number! 🎉';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 25;
+// NOTE: DOM selectors
+const inputGuess = document.querySelector('.guess');
+const again = document.querySelector('.again');
+const check = document.querySelector('.check');
+const number = document.querySelector('.number');
+const body = document.querySelector('body');
 
-// document.querySelector('.guess').value = 1;
+const textElemCl = function (classname, message) {
+  document.querySelector(classname).textContent = message;
+};
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = +document.querySelector('.guess').value;
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No number⛔';
+// NOTE: secret number for a game
+let secretNumber = Math.ceil(Math.random() * 20);
+
+// NOTE: value for DOM score
+let scoreVal = 20;
+let highScoreVal = 0;
+
+// NOTE: check button
+check.addEventListener('click', function () {
+  const guess = +inputGuess.value;
+
+  if (scoreVal > 1) {
+    if (!guess) {
+      // STATE: Empty input
+      textElemCl('.message', 'No number⛔');
+    } else if (guess === secretNumber) {
+      // STATE: WIN
+      textElemCl('.message', 'Correct number! 🎉');
+      textElemCl('.number', secretNumber);
+      body.style.backgroundColor = '#60b347';
+      number.style.width = '30rem';
+      // NOTE: logic for high score
+      if (scoreVal > highScoreVal) {
+        highScoreVal = scoreVal;
+        textElemCl('.highscore', highScoreVal);
+      }
+    } else if (guess !== secretNumber) {
+      // STATE: Too high or too low
+      textElemCl(
+        '.message',
+        guess > secretNumber ? 'Too high 📈' : 'Too low 📉'
+      );
+      scoreVal--;
+      textElemCl('.score', scoreVal);
+    }
+  } else {
+    // STATE: Lost the game
+    textElemCl('.message', 'You lost the game 💥');
+    textElemCl('.score', 0);
   }
+});
+
+// NOTE: again button
+again.addEventListener('click', function () {
+  secretNumber = Math.ceil(Math.random() * 20);
+  scoreVal = 20;
+  textElemCl('.number', '?');
+  textElemCl('.score', scoreVal);
+  textElemCl('.message', 'Start guessing...');
+  inputGuess.value = '';
+  number.style.width = '15rem';
+  body.style.backgroundColor = '#222';
 });
